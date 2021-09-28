a8248220b997bd7952c331649ddfc7d9af9c3f2b
Morad17 <morad123@hotmail.co.uk>
2021-09-28 19:57:29 +0100

    up

--- src/components/projects.js
+++ src/components/projects.js
@@ -6,12 +6,22 @@ const Projects = () => {
     return (
         <section id="projects-section" className="projects-section page">
             <h1 className="project-heading">Project History</h1>
-            <div className="project row">
+            <div className="project-row">
                 <div className="p-row-1">
                     <img src={afkThumb} alt="afk-gamer"/>
+                    <h3>E-Commerce Shop</h3>
+                    <p>An E-Commerce store, selling merchandise like, clothes and accesories. Made with Django and Python, and Javascript funstionality</p>
                 </div>
-                <div className="p-row-2"></div>
-                <div className="p-row-3"></div>
+                <div className="p-row-2">
+                    <img src={afkThumb} alt="afk-gamer"/>
+                    <h3>E-Commerce Shop</h3>
+                    <p>An E-Commerce store, selling merchandise like, clothes and accesories. Made with Django and Python, and Javascript funstionality</p>
+                </div>
+                <div className="p-row-3">
+                <img src={afkThumb} alt="afk-gamer"/>
+                    <h3>E-Commerce Shop</h3>
+                    <p>An E-Commerce store, selling merchandise like, clothes and accesories. Made with Django and Python, and Javascript funstionality</p>
+                </div>
                 <div className="p-row-4"></div>
             </div>
         </section>
