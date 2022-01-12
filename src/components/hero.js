a58d85e32dfe09cd6aec440da035b3cb747d5f4b
Morad17 <morad123@hotmail.co.uk>
2022-01-12 19:18:25 +0000

    updates

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,15 +1,77 @@
-import React from 'react'
+import * as FaIcons from "react-icons/fa"
 
 const Hero = () => {
     return (
         <div className="hero">
-            <h1 className="hero-name">Morad</h1>
-            <h1 className="hero-name">El Bouchikhi</h1>
-            <p className="hero-title">Full Stack Developer</p>
-            <a href="#contact-section" className="basic-button">Get In Touch</a>
+            <div class="stars">
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            </div>
+            <div className="hero-title-row">
+                    <h1 className="hero-name">Morad</h1>
+                    <h1 className="hero-name">El Bouchikhi</h1>
+                    <p className="hero-title">Full Stack Developer</p>
+                    <ul className="social-icons">
+                            <li><a href="https://github.com/Morad17"><FaIcons.FaGithub /></a></li>
+                            <li><a href="/"><FaIcons.FaInstagram /></a></li>
+                            <li><a href="/"><FaIcons.FaLinkedin /></a></li>
+                    </ul>
+                <div className="contact-button">
+                    <a href="#contact-section" className="basic-button">Get In Touch</a>
+                </div>
+            </div>
             <div class="scroll-down">
                 <a href="#about-section" >Scroll Down <span> &#8595;</span></a>
             </div>
+            
         </div>
     )
 }
--- src/components/sideNavbar.js
+++ src/components/sideNavbar.js
@@ -4,20 +4,15 @@ import * as FaIcons from "react-icons/fa"
 const SideNavbar = () => {
     return (
             <nav className="sidenav">
-                <a href="/"><h1 className="logo">ME</h1></a>
                 <ul className="sidenav-icons">
-                    <li><a href="#home"><FaIcons.FaHome /><p>Home</p></a></li>
-                    <li><a href="#about-section"><FaIcons.FaRegUserCircle /><p>About</p></a></li>
-                    <li><a href="#skills-section"><FaIcons.FaCode /><p>Skills</p></a></li>
-                    <li><a href="#projects-section"><FaIcons.FaProjectDiagram /><p>Projects</p></a></li>
-                    <li><a href="#blog-section"><FaIcons.FaBloggerB /><p>Blog</p></a></li>
-                    <li><a href="#contact-section"><FaIcons.FaPaperPlane /><p>Contact</p></a></li>
+                    <li><a href="#home"><FaIcons.FaHome /></a><p>Home</p></li>
+                    <li><a href="#about-section"><FaIcons.FaRegUserCircle /></a><p>About</p></li>
+                    <li><a href="#skills-section"><FaIcons.FaCode /></a><p>Skills</p></li>
+                    <li><a href="#projects-section"><FaIcons.FaProjectDiagram /></a><p>Projects</p></li>
+                    <li><a href="#blog-section"><FaIcons.FaBloggerB /></a><p>Blog</p></li>
+                    <li><a href="#contact-section"><FaIcons.FaPaperPlane /></a><p>Contact</p></li>
                 </ul>
-                <ul className="social-icons">
-                    <li><a href="/"><FaIcons.FaGithub /></a></li>
-                    <li><a href="/"><FaIcons.FaInstagram /></a></li>
-                    <li><a href="/"><FaIcons.FaLinkedin /></a></li>
-                </ul>
+                
             </nav>
     )
 }
