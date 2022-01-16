918ad1153b4d533deafafe456f0d6d1f038b5426
Morad17 <morad123@hotmail.co.uk>
2022-01-16 20:07:31 +0000

    sorting out about me

--- src/components/about.js
+++ src/components/about.js
@@ -5,7 +5,10 @@ import profilePic from '../assets/images/profile-pic.png'
 const About = () => {
     return (
         <section id="about-section" className="about-section page">
+                <div className="box-wrapper-1"></div>
+                <div className="box-wrapper-2"></div>
             <div className="about-text">
+                
                 <h1 className="main-title">About Me</h1>
                 <p className="main-text">
                     Hello there. I am Morad an aspiring full stack web developer. 
