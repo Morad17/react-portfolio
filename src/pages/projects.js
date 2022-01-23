c43e23f8922cd873c957f826a689b4471b0a456a
Morad17 <morad123@hotmail.co.uk>
2022-01-23 22:21:04 +0000

    updates

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -7,54 +7,55 @@ import * as DiIcons from "react-icons/di"
 const Projects = () => {
     return (
         <section id="projects-section" className="projects-section page">
-            <div className="p-heading-row">
-                <h1 className="project-heading">Project</h1>
-                <h1 className="project-heading">History</h1>
-            </div>
             <div className="project-row">
-                <h2>Latest Projects</h2>
+                <h2 className="project-heading">Latest Projects</h2>
                 <div className="project-card-row">
-                    
-                    <div className="project-card-1"> 
+                    <div className="project-card-1">
+                        <h3 className="project-header">AFK Gamer Store</h3>
                         <img src={afkThumb} alt="afk-gamer"/>
-                        <h3>E-Commerce Shop</h3>
-                        <p>An E-Commerce store, selling merchandise like, clothes and accesories. Made with Django and Python, and Javascript funstionality</p>
-                        <div className="project-icon">
-                            <DiIcons.DiDjango />
-                            <FaIcons.FaPython />
-                            <DiIcons.DiJavascript1 />
+                        <div className="project-details">
+                            
+                            <div className="project-icon">
+                                <DiIcons.DiDjango />
+                                <FaIcons.FaPython />
+                                <DiIcons.DiJavascript1 />
+                            </div>
+                            <div className="code-links">
+                                <a href="#" className="project-button">Code</a>
+                                <a href="#" className="project-button">Project</a>
+                            </div>
                         </div>
-                        <div className="code-links">
-                            <a href="#" className="button-1">Code</a>
-                            <a href="#" className="button-1">Project</a>
-                        </div>   
                     </div>
                     <div className="project-card-2">
+                        <h3 className="project-header">AFK Gamer Store</h3>
                         <img src={afkThumb} alt="afk-gamer"/>
-                        <h3>React Blog</h3>
-                        <p>An E-Commerce store, selling merchandise like, clothes and accesories. Made with Django and Python, and Javascript funstionality</p>
-                        <div className="project-icon">
-                            <FaIcons.FaReact />
-                            <DiIcons.DiJavascript1 />
-                            <DiIcons.DiMongodb />
-                        </div>
-                        <div className="code-links">
-                            <a href="#" className="button-1">Code</a>
-                            <a href="#" className="button-1">Project</a>
+                        <div className="project-details">
+                            
+                            <div className="project-icon">
+                                <DiIcons.DiDjango />
+                                <FaIcons.FaPython />
+                                <DiIcons.DiJavascript1 />
+                            </div>
+                            <div className="code-links">
+                                <a href="#" className="project-button">Code</a>
+                                <a href="#" className="project-button">Project</a>
+                            </div>
                         </div>
                     </div>
                     <div className="project-card-3">
-                    <img src={afkThumb} alt="afk-gamer"/>
-                        <h3>Travel App</h3>
-                        <p>An E-Commerce store, selling merchandise like, clothes and accesories. Made with Django and Python, and Javascript funstionality</p>
-                        <div className="project-icon">
-                            <FaIcons.FaReact />
-                            <DiIcons.DiJavascript1 />
-                            <DiIcons.DiMongodb />
-                        </div>
-                        <div className="code-links">
-                            <a href="#" className="button-1">Code</a>
-                            <a href="#" className="button-1">Project</a>
+                        <h3 className="project-header">AFK Gamer Store</h3>
+                        <img src={afkThumb} alt="afk-gamer"/>
+                        <div className="project-details">
+                            
+                            <div className="project-icon">
+                                <DiIcons.DiDjango />
+                                <FaIcons.FaPython />
+                                <DiIcons.DiJavascript1 />
+                            </div>
+                            <div className="code-links">
+                                <a href="#" className="project-button">Code</a>
+                                <a href="#" className="project-button">Project</a>
+                            </div>
                         </div>
                     </div>
                     <div className="project-card-4"></div>
