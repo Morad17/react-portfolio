5e96516007d5b8bc8944ebd8cd096f86b5be74aa
Morad17 <morad123@hotmail.co.uk>
2022-02-06 23:29:58 +0000

    updates

--- src/components/mobileNavbar.js
+++ src/components/mobileNavbar.js
@@ -33,17 +33,17 @@ const MobileNavbar = () => {
             </div>
             <ul className="mobilenav-dropdown">
                 <hr />
-                <li><a href="#home"><FaIcons.FaHome /></a><p>Home</p></li>
+                <li><a href="#home"><FaIcons.FaHome /><p>Home</p></a></li>
                 <hr />
-                <li><a href="#about-section"><FaIcons.FaRegUserCircle /></a><p>About</p></li>
+                <li><a href="#about-section"><FaIcons.FaRegUserCircle /><p>About</p></a></li>
                 <hr />
-                <li><a href="#skills-section"><FaIcons.FaCode /></a><p>Skills</p></li>
+                <li><a href="#skills-section"><FaIcons.FaCode /><p>Skills</p></a></li>
                 <hr />
-                <li><a href="#projects-section"><FaIcons.FaProjectDiagram /></a><p>Projects</p></li>
+                <li><a href="#projects-section"><FaIcons.FaProjectDiagram /><p>Projects</p></a></li>
                 <hr />
-                <li><a href="#blog-section"><FaIcons.FaBloggerB /></a><p>Blog</p></li>
+                <li><a href="#blog-section"><FaIcons.FaBloggerB /><p>Blog</p></a></li>
                 <hr />
-                <li><a href="#contact-section"><FaIcons.FaPaperPlane /></a><p>Contact</p></li>
+                <li><a href="#contact-section"><FaIcons.FaPaperPlane /><p>Contact</p></a></li>
                 <hr />
             </ul>
         </nav>
